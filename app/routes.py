8f707df23a45e8a2714207694a1e34a336013868
Shubhrajit1pallob <spallob@asu.edu>
2025-05-16 16:21:03 -0700

    Completed a version 1 of my portfolio website.

---
+++ app/routes.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/python_projects")
+def python_projects():
+    return render_template("python_project.html")
+
+@app.route("/web_dev")
+def web_dev():
+    return render_template("web_dev.html")
+
+@app.route("/android_development")
+def android_development():
+    return render_template("android_development.html")
+
+@app.route("/cyber_security")
+def cyber_security():
+    return render_template("cybersecurity_project.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -4,3 +4,10 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+var typed = new Typed(".typed_text", {
+  strings: ["Software Developer", "Cybersecurity Engineer", "Security Analyst", "DevSecOps Engineer"],
+  typeSpeed: 50,
+  backSpeed: 80,
+  loop: true
+});
